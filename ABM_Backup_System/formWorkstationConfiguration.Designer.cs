1041c726d3056230c13e95c7751174b2325eda97
Max Allan-Smith <maxibon_1195@hotmail.com>
2022-10-09 21:10:13 +0100

    formWorkstationConfiguration added

---
+++ ABM_Backup_System/formWorkstationConfiguration.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace ABM_Backup_System
+{
+    partial class formWorkstationConfiguration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip_BottomMenu = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton_Exit = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton_Help = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton_Sell = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton_Cancel = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.label_ServerPath = new System.Windows.Forms.Label();
+            this.textBox_ServerPath = new System.Windows.Forms.TextBox();
+            this.label_ServerName = new System.Windows.Forms.Label();
+            this.textBox_ServerName = new System.Windows.Forms.TextBox();
+            this.label_DatabaseName = new System.Windows.Forms.Label();
+            this.textBox_DatabaseName = new System.Windows.Forms.TextBox();
+            this.toolStrip_BottomMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip_BottomMenu
+            // 
+            this.toolStrip_BottomMenu.AutoSize = false;
+            this.toolStrip_BottomMenu.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip_BottomMenu.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip_BottomMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton_Exit,
+            this.toolStripSeparator1,
+            this.toolStripButton_Help,
+            this.toolStripSeparator2,
+            this.toolStripButton_Sell,
+            this.toolStripButton_Cancel,
+            this.toolStripSeparator4});
+            this.toolStrip_BottomMenu.Location = new System.Drawing.Point(0, 254);
+            this.toolStrip_BottomMenu.Name = "toolStrip_BottomMenu";
+            this.toolStrip_BottomMenu.Size = new System.Drawing.Size(377, 96);
+            this.toolStrip_BottomMenu.TabIndex = 22;
+            this.toolStrip_BottomMenu.Text = "toolStrip1";
+            // 
+            // toolStripButton_Exit
+            // 
+            this.toolStripButton_Exit.AutoSize = false;
+            this.toolStripButton_Exit.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.toolStripButton_Exit.Image = global::ABM_Backup_System.Properties.Resources.Exit;
+            this.toolStripButton_Exit.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton_Exit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_Exit.Name = "toolStripButton_Exit";
+            this.toolStripButton_Exit.Size = new System.Drawing.Size(50, 61);
+            this.toolStripButton_Exit.Text = "Exit";
+            this.toolStripButton_Exit.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.toolStripButton_Exit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 96);
+            // 
+            // toolStripButton_Help
+            // 
+            this.toolStripButton_Help.AutoSize = false;
+            this.toolStripButton_Help.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.toolStripButton_Help.Image = global::ABM_Backup_System.Properties.Resources.question_mark;
+            this.toolStripButton_Help.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton_Help.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_Help.Name = "toolStripButton_Help";
+            this.toolStripButton_Help.Size = new System.Drawing.Size(50, 61);
+            this.toolStripButton_Help.Text = "Help";
+            this.toolStripButton_Help.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.toolStripButton_Help.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 96);
+            // 
+            // toolStripButton_Sell
+            // 
+            this.toolStripButton_Sell.AutoSize = false;
+            this.toolStripButton_Sell.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.toolStripButton_Sell.Image = global::ABM_Backup_System.Properties.Resources.OK;
+            this.toolStripButton_Sell.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton_Sell.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_Sell.Name = "toolStripButton_Sell";
+            this.toolStripButton_Sell.Size = new System.Drawing.Size(50, 61);
+            this.toolStripButton_Sell.Text = "Sell";
+            this.toolStripButton_Sell.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.toolStripButton_Sell.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButton_Cancel
+            // 
+            this.toolStripButton_Cancel.AutoSize = false;
+            this.toolStripButton_Cancel.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.toolStripButton_Cancel.Image = global::ABM_Backup_System.Properties.Resources.Cancel;
+            this.toolStripButton_Cancel.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton_Cancel.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_Cancel.Name = "toolStripButton_Cancel";
+            this.toolStripButton_Cancel.Size = new System.Drawing.Size(50, 61);
+            this.toolStripButton_Cancel.Text = "Cancel";
+            this.toolStripButton_Cancel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.toolStripButton_Cancel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 96);
+            // 
+            // label_ServerPath
+            // 
+            this.label_ServerPath.AutoSize = true;
+            this.label_ServerPath.Location = new System.Drawing.Point(22, 40);
+            this.label_ServerPath.Name = "label_ServerPath";
+            this.label_ServerPath.Size = new System.Drawing.Size(82, 17);
+            this.label_ServerPath.TabIndex = 23;
+            this.label_ServerPath.Text = "Server Path:";
+            // 
+            // textBox_ServerPath
+            // 
+            this.textBox_ServerPath.Location = new System.Drawing.Point(22, 60);
+            this.textBox_ServerPath.Name = "textBox_ServerPath";
+            this.textBox_ServerPath.Size = new System.Drawing.Size(329, 25);
+            this.textBox_ServerPath.TabIndex = 24;
+            // 
+            // label_ServerName
+            // 
+            this.label_ServerName.AutoSize = true;
+            this.label_ServerName.Location = new System.Drawing.Point(22, 98);
+            this.label_ServerName.Name = "label_ServerName";
+            this.label_ServerName.Size = new System.Drawing.Size(90, 17);
+            this.label_ServerName.TabIndex = 25;
+            this.label_ServerName.Text = "Server Name:";
+            // 
+            // textBox_ServerName
+            // 
+            this.textBox_ServerName.Location = new System.Drawing.Point(22, 118);
+            this.textBox_ServerName.Name = "textBox_ServerName";
+            this.textBox_ServerName.Size = new System.Drawing.Size(329, 25);
+            this.textBox_ServerName.TabIndex = 26;
+            // 
+            // label_DatabaseName
+            // 
+            this.label_DatabaseName.AutoSize = true;
+            this.label_DatabaseName.Location = new System.Drawing.Point(22, 156);
+            this.label_DatabaseName.Name = "label_DatabaseName";
+            this.label_DatabaseName.Size = new System.Drawing.Size(107, 17);
+            this.label_DatabaseName.TabIndex = 27;
+            this.label_DatabaseName.Text = "Database Name:";
+            // 
+            // textBox_DatabaseName
+            // 
+            this.textBox_DatabaseName.Location = new System.Drawing.Point(22, 176);
+            this.textBox_DatabaseName.Name = "textBox_DatabaseName";
+            this.textBox_DatabaseName.Size = new System.Drawing.Size(329, 25);
+            this.textBox_DatabaseName.TabIndex = 28;
+            // 
+            // formWorkstationConfiguration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 350);
+            this.Controls.Add(this.textBox_DatabaseName);
+            this.Controls.Add(this.label_DatabaseName);
+            this.Controls.Add(this.textBox_ServerName);
+            this.Controls.Add(this.label_ServerName);
+            this.Controls.Add(this.textBox_ServerPath);
+            this.Controls.Add(this.label_ServerPath);
+            this.Controls.Add(this.toolStrip_BottomMenu);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formWorkstationConfiguration";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Workstation Configuration";
+            this.toolStrip_BottomMenu.ResumeLayout(false);
+            this.toolStrip_BottomMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip_BottomMenu;
+        private ToolStripButton toolStripButton_Exit;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripButton toolStripButton_Help;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripButton toolStripButton_Sell;
+        private ToolStripButton toolStripButton_Cancel;
+        private ToolStripSeparator toolStripSeparator4;
+        private Label label_ServerPath;
+        private TextBox textBox_ServerPath;
+        private Label label_ServerName;
+        private TextBox textBox_ServerName;
+        private Label label_DatabaseName;
+        private TextBox textBox_DatabaseName;
+    }
+}
